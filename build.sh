3f3f67e4db828d412a8cc3ec743cf56f7a59a8bc
meow <you@example.com>
2019-12-14 14:50:14 +0900

    Macでも複数プロセスでビルドできるようにした

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ mkdir -p $install_dir
 # nvxs
 cd nvxs
 ./configure --prefix $install_dir --enable-openmp
-make all -j$(nproc)
+make all -j$(getconf _NPROCESSORS_ONLN)
 make install
 cd ${cdir}
 
 # ruby ext
 cd animeface-ruby
 ruby extconf_local.rb
-make -j$(nproc)
+make -j$(getconf _NPROCESSORS_ONLN)
 
 echo -n "\n\nCheck:"
 echo " % cd animeface-ruby"
